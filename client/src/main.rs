843fef89fc0c65d06e8ec50f859d824662c5016f
mikecat1024 <kensuke_seki@demia.co.jp>
2026-02-07 13:26:21 +0900

    remove unused dependency features

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,19 +1,26 @@
 use client::NotionClient;
-use dotenvy::dotenv;
 use notion_to_markdown_core::BlockChildren;
 use std::{env, fs};
 
 const OUTPUT_PATH: &str = "output.md";
 const TOKEN_ENV_VAR: &str = "NOTION_TOKEN";
 
-#[tokio::main]
-async fn main() {
+#[cfg(feature = "log")]
+fn init_cli_environment() {
     env_logger::init();
-    dotenv().ok();
+    dotenvy::dotenv().ok();
+}
+
+#[cfg(not(feature = "log"))]
+fn init_cli_environment() {}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    init_cli_environment();
     let token = env::var(TOKEN_ENV_VAR).expect("NOTION_TOKEN is not set");
 
     let markdown = NotionClient::new(token)
-        .retrieve_block_children("2ed53222ff1e800390c6d4d47f1c12fe".into(), None, None)
+        .retrieve_block_children("2f853222ff1e80829678eeb55e7add95".into(), None, None)
         .await
         .unwrap()
         .to_markdown(0);
